9756e8cefb32e9108a2aac8cc25a9121b1d3cc87
Charlie Weems <charlie@codeforamerica.org>
2017-11-13 10:23:59 -0800

    Add route for staticfiles

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from django.contrib import admin
+from django.conf import settings
 from main import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
