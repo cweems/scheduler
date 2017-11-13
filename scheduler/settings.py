af5f02dd0585a454622fa5c1f968309c1c907ba0
Charlie Weems <charlie@codeforamerica.org>
2017-11-12 23:27:51 -0800

    Retry staticfiles settings

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -131,8 +131,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATIC_URL = '/static/'
